6ff32dec24432838025756e61a8bdb00f130cb98
IntelMin <intelmin0915@gmail.com>
2022-01-06 21:43:33 +0800

    race commit

--- contracts/Race.sol
+++ contracts/Race.sol
@@ -1,197 +1,190 @@
 // SPDX-License-Identifier: MIT
-
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "./DogNFT.sol";
+import "./FoodNFT.sol";
+import "./libraries/DogLib.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-// Staking distributes the CRACE rewards based on staked CRACE to each user.
+contract Race {
 
-contract Staking is Ownable {
-    using SafeERC20 for IERC20;
+  using Counters for Counters.Counter;
+  mapping(uint256 => bool) isRaceExist;
 
-    // Info of each user.
-    struct UserInfo {
-        uint256 amount;     // How many CRACE tokens the user has provided.
-        uint256 rewardDebt; // Reward debt. See explanation below.
-        uint256 timestamp;
+  address[] public dailyParticipant;
+  address[] public tourParticipant;
+  address[] public championParticipant;
+  mapping(uint256 => uint256) count;  //how many participants per each race
+  mapping(uint256 => mapping(uint256 => uint256)) players;  //raceType => id => dogID
+  mapping(uint256 => mapping(uint256 => DogLib.DogBaseInfo)) playerBase;   //racetype => dogID => ...
+  mapping(uint256 => mapping(uint256 => address)) dogOwners;  //raceType => dogID => ownerAdress
+  mapping(uint256 => mapping(uint256 => DogLib.extraTrait)) extra; //raceType => dogID => extraTrait
+  mapping(uint256 => uint256) extraAgility; //dogID => extraAgility      #when good event occurs during race
+  uint256[] public dDogID;  //dogIDs of daily race players
+  uint256[] public tDogID;  //dogIDs of tournament race players
+  uint256[] public cDogID;  //dogIDs of champaign race players
+  uint256[3] raceRound;  //daily:1   tournament:3    championShip:6  constant
+  uint256[3] requiredStam;  //required stamina to join in the race   daily:15   tournament:20    championShip:25
+  mapping(uint256 => uint256) currentRound;   //
+  DogLib.DogTrait[] dTrait; //trait of daily race players
+  DogLib.DogTrait[] tTrait; //trait of tournament race players
+  DogLib.DogTrait[] cTrait; //trait of champaign race players
+  DogNFT dog;
+  //  racetype=0 : dailyRace  1: tournament 2: championShip
+  struct dailyResult {
+    uint256 dogID;
+    uint256 ability;
+  }
+
+  struct tournamentResult {
+    uint256 dogID;
+    uint256 ability;
+  }
+
+  struct champignResult {
+    uint256 dogID;
+    uint256 ability;
+  }
+
+  dailyResult[] dResult;
+  tournamentResult[] tResult;
+  champignResult[] cResult;
+
+  constructor() {
+    raceRound[0] = 1;
+    raceRound[1] = 3;
+    raceRound[2] = 6;
+    requiredStam[0] = 15;
+    requiredStam[1] = 20;
+    requiredStam[2] = 25;
+  }
+
+  function participate(uint256 dogID, uint256 raceType) public {
+    require(isRaceExist[raceType] == true, "No race organized");
+    require(count[raceType]<12, "Full participants");
+    require(dog.getDogStamina(dogID) >= requiredStam[raceType], "Your dog is too tired to race");
+
+    if(raceType == 0) { 
+      dDogID.push(dogID);
+      dTrait.push(dog.getDogTrait(dogID));
+    }
+    else if(raceType == 1) { 
+      tDogID.push(dogID); 
+      tTrait.push(dog.getDogTrait(dogID)); 
+    }
+    else { 
+      cDogID.push(dogID); 
+      cTrait.push(dog.getDogTrait(dogID)); 
     }
 
-    //a year: 31536000 secs
+    uint256 index;
+    index = count[raceType];
+  //  players[raceType][count] = dogID;
+    count[raceType]++;
+  }
 
-    // Info of each pool.
-    struct PoolInfo {
-        uint256 lockTime;           // a month: 2592000 secs
-        uint256 apy;                // APY
-        uint256 withdrawFee;        // Fee percentage for withdrawing anytime
-        uint256 stakedAmount;
-        uint256 compoundedAmount;
+  function organizeRace(uint256 raceType) public {
+    isRaceExist[raceType] = true;
+  }
+
+  function startRace(uint256 raceType) public {
+    require(count[raceType] >= 10, "Not enough players");
+    raceMain(raceType);
+    
+  }
+
+  function raceMain(uint256 raceType) public {
+    require(currentRound[raceType] < raceRound[raceType], "Race over");
+    uint256[] memory dTotalResult;
+    uint256[] memory tTotalResult;
+    uint256[] memory cTotalResult;
+    uint256 dWinner;
+    uint256 tWinner;
+    uint256 cWinner;
+    if(raceType == 0) {
+      for( uint256 index = 0; index < dTrait.length; index++ ) {
+        if(DogLib.chance(dTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        dTotalResult[index] = (dTrait[index].agility + extraAgility[index]) * dTrait[index].stamina * dTrait[index].health / dTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < dTrait.length; index++ ) {
+        if(DogLib.chance(dTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        dTotalResult[index] = dTotalResult[index] + (dTrait[index].agility + extraAgility[index]) * dTrait[index].stamina * dTrait[index].health / dTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < dTrait.length; index++ ) {
+        if(DogLib.chance(dTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        dTotalResult[index] = dTotalResult[index] + (dTrait[index].agility + extraAgility[index]) * dTrait[index].stamina * dTrait[index].health / dTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      dWinner = dDogID[DogLib.pickMax(dTotalResult)];
     }
 
-    // Address of the CRACE Token contract.
-    IERC20 public crace;
-    // The total amount of CRACE that's paid out as reward.
-    uint256 public paidOut = 0;
-
-    // The total amount for rewards.
-    uint256 public rewardsAmount = 0;
-
-    // Total Withdraw Fee Amount
-    uint256 public feeAmount = 0;
-    // Info of each pool.
-    PoolInfo[] public poolInfo;
-    // Info of each user that stakes BEP20 tokens.
-    mapping (uint256 => mapping (address => UserInfo)) public userInfo;
-
-    event ClaimReward(address indexed user, uint256 indexed pid, uint256 amount);
-    event Compound(address indexed user, uint256 indexed pid, uint256 amount);
-    event Deposit(address indexed user, uint256 indexed pid, uint256 amount);
-    event Withdraw(address indexed user, uint256 indexed pid, uint256 amount);
-    event EmergencyWithdraw(address indexed user, uint256 indexed pid, uint256 amount);
-
-    constructor(IERC20 _crace) {
-        crace = _crace;
+    else if(raceType == 1) {
+      for( uint256 index = 0; index < tTrait.length; index++ ) {
+        if(DogLib.chance(tTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        tTotalResult[index] = (tTrait[index].agility + extraAgility[index]) * tTrait[index].stamina * tTrait[index].health / tTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < tTrait.length; index++ ) {
+        if(DogLib.chance(tTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        tTotalResult[index] = tTotalResult[index] + (tTrait[index].agility + extraAgility[index]) * tTrait[index].stamina * tTrait[index].health / tTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < tTrait.length; index++ ) {
+        if(DogLib.chance(tTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        tTotalResult[index] = tTotalResult[index] + (tTrait[index].agility + extraAgility[index]) * tTrait[index].stamina * tTrait[index].health / tTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      tWinner = tDogID[DogLib.pickMax(tTotalResult)];
     }
 
-    // Number of staking pools
-    function poolLength() external view returns (uint256) {
-        return poolInfo.length;
+    else {
+      for( uint256 index = 0; index < cTrait.length; index++ ) {
+        if(DogLib.chance(cTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        cTotalResult[index] = (cTrait[index].agility + extraAgility[index]) * cTrait[index].stamina * cTrait[index].health / cTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < cTrait.length; index++ ) {
+        if(DogLib.chance(cTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        cTotalResult[index] = cTotalResult[index] + (cTrait[index].agility + extraAgility[index]) * cTrait[index].stamina * cTrait[index].health / cTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      for( uint256 index = 0; index < cTrait.length; index++ ) {
+        if(DogLib.chance(cTrait[index].luck)) getAgility(index);
+        else getInjured(raceType, index);
+        cTotalResult[index] = cTotalResult[index] + (cTrait[index].agility + extraAgility[index]) * cTrait[index].stamina * cTrait[index].health / cTrait[index].weight;
+        extraAgility[index] = 0;
+      }
+      cWinner = cDogID[DogLib.pickMax(cTotalResult)];
     }
+  }
 
-    // Fund the Staking
-    function fund(uint256 _amount) external onlyOwner {
-        rewardsAmount = rewardsAmount + _amount;
-        crace.transferFrom(address(msg.sender), address(this), _amount);
-    }
+  function getAgility(uint256 index) public {
+    extraAgility[index] = 20;
+  }
 
-    function add(uint256 _lockTime, uint256 _apy, uint256 _withdrawFee) external onlyOwner {
-        poolInfo.push(PoolInfo({
-            lockTime: _lockTime,
-            apy: _apy,
-            withdrawFee: _withdrawFee,
-            stakedAmount: 0,
-            compoundedAmount: 0
-        }));
-    }
-
-    function updatePool(uint256 _pid, uint256 _lockTime, uint256 _apy, uint256 _withdrawFee) external onlyOwner {
-        poolInfo[_pid].lockTime = _lockTime;
-        poolInfo[_pid].apy = _apy;
-        poolInfo[_pid].withdrawFee = _withdrawFee;
-    }
-
-    // View function to see deposited BEP20 for a user.
-    function deposited(uint256 _pid, address _user) external view returns (uint256) {
-        UserInfo storage user = userInfo[_pid][_user];
-        return user.amount;
-    }
-
-    function pending(uint256 _pid, address _user) external view returns (uint256) {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][_user];
-
-        return user.amount * pool.apy * (block.timestamp - user.timestamp) / 3153600000 + user.rewardDebt;
-    }
-
-    function claimReward(uint256 _pid) external {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(block.timestamp - user.timestamp >= pool.lockTime, "claim: locked");
-        uint256 pendingAmount = user.amount * pool.apy * (block.timestamp - user.timestamp) / 3153600000 + user.rewardDebt;
-        require(pendingAmount <= rewardsAmount, "Not enough rewards amount");
-        user.timestamp = block.timestamp;
-        user.rewardDebt = 0;
-        paidOut += pendingAmount;
-        rewardsAmount = rewardsAmount - pendingAmount;
-        crace.safeTransfer(address(msg.sender), pendingAmount);
-        emit ClaimReward(msg.sender, _pid, pendingAmount);
-    }
-
-    function compound(uint256 _pid) external {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        uint256 pendingAmount = user.amount * pool.apy * (block.timestamp - user.timestamp) / 3153600000 + user.rewardDebt;
-        require(pendingAmount > 0, "compound: wrong amount");
-        require(pendingAmount <= rewardsAmount, "Not enough rewards amount");
-        rewardsAmount = rewardsAmount - pendingAmount;
-        user.rewardDebt = 0;
-        user.timestamp = block.timestamp;
-        user.amount = user.amount + pendingAmount;
-        paidOut += pendingAmount;
-        pool.stakedAmount = pool.stakedAmount + pendingAmount;
-        pool.compoundedAmount = pool.compoundedAmount + pendingAmount;
-        emit Compound(msg.sender, _pid, pendingAmount);
-    }
-
-    function deposit(uint256 _pid, uint256 _amount) external {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(_amount > 0, "deposit: wrong amount");
-        crace.safeTransferFrom(address(msg.sender), address(this), _amount);
-        user.rewardDebt = user.amount * pool.apy * (block.timestamp - user.timestamp) / 3153600000 + user.rewardDebt;
-        user.timestamp = block.timestamp;
-        user.amount = user.amount + _amount;
-        pool.stakedAmount = pool.stakedAmount + _amount;
-        emit Deposit(msg.sender, _pid, _amount);
-    }
-
-    function withdraw(uint256 _pid, uint256 _amount) external {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(block.timestamp - user.timestamp >= pool.lockTime, "withdraw: locked");
-        require(user.amount >= _amount && _amount > 0, "withdraw: wrong amount");
-        user.rewardDebt = user.amount * pool.apy * (block.timestamp - user.timestamp) / 3153600000 + user.rewardDebt;
-        user.timestamp = block.timestamp;
-        user.amount = user.amount - _amount;
-        pool.stakedAmount = pool.stakedAmount - _amount;
-        crace.safeTransfer(address(msg.sender), _amount);
-        emit Withdraw(msg.sender, _pid, _amount);
-    }
-
-    // Withdraw without caring about rewards. EMERGENCY ONLY.
-    function emergencyWithdraw(uint256 _pid) external {
-        PoolInfo storage pool = poolInfo[_pid];
-        UserInfo storage user = userInfo[_pid][msg.sender];
-        require(block.timestamp - user.timestamp < pool.lockTime, "withdraw: unlocked");
-        pool.stakedAmount = pool.stakedAmount - user.amount;
-        uint256 amount = user.amount * (100 - pool.withdrawFee) / 100;
-        feeAmount = feeAmount + (user.amount * (pool.withdrawFee / 100));
-        user.amount = 0;
-        user.timestamp = block.timestamp;
-        user.rewardDebt = 0;
-        crace.safeTransfer(address(msg.sender), amount);
-        emit EmergencyWithdraw(msg.sender, _pid, user.amount);
-    }
-
-    //Get total amount of withdraw fee
-    function getWithdrawFeeAmount() external view returns(uint256) {
-        return feeAmount;
-    }
-
-    //Add withdraw fee amount to rewards amount
-    function addWithdrawFeeToRewards() external onlyOwner {
-        rewardsAmount = rewardsAmount + feeAmount;
-        feeAmount = 0;
-    }
-
-    //Withdraw fee amount to address _to
-    function withdrawFeeAmount(address _to) external onlyOwner {
-        require(feeAmount > 0, "Not enough Fee Amount");   
-        crace.safeTransferFrom(address(this), _to, feeAmount);
-        feeAmount = 0;
-    }
-
-    //Withdraw rewards amount to address _to
-    function withdrawRewards(address _to) external onlyOwner {
-        require(rewardsAmount > 0, "Not enough Rewards Amount");
-        crace.safeTransferFrom(address(this), _to, rewardsAmount);
-        rewardsAmount = 0;
-    }
-
-    //Withraw total amount of contract to address _to
-    function withdrawFunds(address _to) external onlyOwner {
-        uint256 balance = crace.balanceOf(address(this));
-        crace.safeTransfer(_to, balance);
-    }
+  function getInjured(uint256 raceType, uint256 index) public {
+    uint256 dogID;
+    if(raceType == 0) dogID = dDogID[index];
+    else if(raceType == 1) dogID = tDogID[index];
+    else dogID = cDogID[index]; 
+    //dog.dogTrait[dogID].health = dog.dogTrait[dogID].health - 10;
+    dog.decreaseHealth(dogID);
+    if(raceType == 0)
+      dTrait[index].health = dTrait[index].health - 10;
+    else if(raceType == 1)
+      tTrait[index].health = tTrait[index].health - 10;
+    else
+      cTrait[index].health = cTrait[index].health - 10;
+  }
 }
